ff87a79e678cfb8a9c0340903a5a49ec138af993
Afalkenhagen <ahau_buluk@protonmail.ch>
2024-02-06 20:43:48 -0300

    chore: added links

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/hero/hero.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, FooterComponent],
+  imports: [HeroComponent, FooterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
